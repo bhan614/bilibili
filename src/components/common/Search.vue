e71055a35846675e9efdf13089c89135ea1f420d
bhan614 <985900385@qq.com>
2017-05-15 15:58:05 +0800

    update header banner and add search

---
+++ src/components/common/Search.vue
@@ -0,0 +1,19 @@
+<template>
+	<div class="search">
+
+	</div>
+</template>
+<script>
+	export default {
+	}
+</script>
+
+<style lang="stylus">
+	.search
+		width 100px
+		height 32px
+		background red
+		position absolute
+		top calc(170px - (56px - 32px) - 32px)
+		right 0px
+</style>
