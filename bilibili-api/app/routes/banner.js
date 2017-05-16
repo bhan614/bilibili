33e9a1a0ff571f82a6aa8008a8a68246db5f152a
bhan614 <985900385@qq.com>
2017-05-16 16:09:43 +0800

    add koa

---
+++ bilibili-api/app/routes/banner.js
@@ -0,0 +1,16 @@
+import Router from 'koa-router'
+import axios from 'axios'
+const router = Router({
+  prefix: '/banner'
+})
+router.post('/list', async (ctx, next) => {
+  let response = await axios.get('')
+  ctx.body = response
+})
+
+router.get('/list', async (ctx, next) => {
+  //let response = await axios.get('http://api.bilibili.com/x/web-show/res/loc?callback=jQuery17205969745067413896_1482805801285&jsonp=jsonp&pf=0&id=23&_=1482805801599')
+  ctx.body = 1
+})
+
+export default router
---
+++ bilibili-api/app/routes/urlConfig.js
@@ -0,0 +1,59 @@
+// 各分类内容
+export const ding = 'http://www.bilibili.com/index/ding.json'
+
+
+// 轮播图
+export const banner = 'http://api.bilibili.com/x/web-show/res/loc?callback=jQuery17205969745067413896_1482805801285&jsonp=jsonp&pf=0&id=23&_=1482805801599'
+
+// 默认搜索单词
+export const getSearchDefaultWords = 'http://www.bilibili.com/widget/getSearchDefaultWords'
+
+// 主站头部背景图
+export const topbg = 'http://api.bilibili.com/x/web-show/res/loc?callback=jQuery1720029742745197591347_1488209619074&pf=0&id=142&jsonp=jsonp&_=1488209619792'
+
+// 各分类热门内容
+export const hot = 'http://api.bilibili.com/x/web-show/res/locs?callback=jQuery1720029742745197591347_1488209619075&pf=0&ids=1550%2C1554%2C1556%2C1558%2C1560%2C1562%2C1564%2C1566%2C1568%2C1570%2C1572%2C1574%2C1624%2C1636&jsonp=jsonp&_=1488209619803'
+
+// 推广
+export const promote = 'http://api.bilibili.com/x/web-show/res/loc?callback=jQuery1720029742745197591347_1488209619076&jsonp=jsonp&pf=0&id=34&_=1488209619911'
+
+// 推广右侧广告
+export const promoteAd = 'http://api.bilibili.com/x/web-show/res/loc?callback=jQuery1720029742745197591347_1488209619077&jsonp=jsonp&pf=0&id=29&_=1488209619914'
+
+// 直播
+export const liveXhr = 'http://live.bilibili.com/bili/recom?callback=liveXhrDone'
+
+// 番剧下方更新列表右侧新番放送表上侧
+export const slideshow = 'http://www.bilibili.com/index/slideshow/41.json'
+// 番剧下方更新列表右侧新番放送表下侧
+export const season = 'http://www.bilibili.com/api_proxy?app=bangumi&action=get_season_by_tag&page=1&tag_id=101&pagesize=4'
+
+
+// 轮播图右边总排行
+// 3日排行
+export const ranking3 = 'http://www.bilibili.com/index/ranking-3day.json'
+// 7日排行
+export const ranking7 = 'http://www.bilibili.com/index/ranking-week.json'
+// 昨日排行
+export const randking = 'http://www.bilibili.com/index/ranking.json'
+
+// 最底部特别推荐
+export const recommend = 'http://www.bilibili.com/index/recommend.json'
+
+// 各分类下3日排行 http://www.bilibili.com/index/catalogy/
+// 动画排行 1-3day.json
+// 番剧排行 13-3day.json
+// 音乐排行 3-3day.json
+// 舞蹈排行 129-3day.json
+// 游戏排行 3-3day.json
+// 科技排行 36-3day.json
+// 生活排行 160-3day.json
+// 鬼畜排行 119-3day.json
+// 时尚排行 155-3day.json
+// 广告排行 165-3day.json
+// 娱乐排行 5-3day.json
+// 电影排行 23-3day.json
+// TV剧集排行 11-3day.json
+
+// 各分类下7日排行
+// 动画排行 1-week.json
